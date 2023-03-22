52c286bdaf4761578ca36af8acee1101ff04eca9
houdamzari <houda.mzari1999@gmail.com>
2023-03-22 23:52:50 +0000

    Fix linter issues

--- genre.rb
+++ genre.rb
@@ -12,16 +12,17 @@ class Genre
     @items << item
     item.genre = self
   end
-    def to_json(*args)
+
+  def to_json(*args)
     {
       'name' => @name,
       'items' => @items.map(&:to_json)
     }.to_json(*args)
   end
-  
+
   def self.from_json(json)
     data = JSON.parse(json)
-    genre = self.new(data['name'])
+    genre = new(data['name'])
     genre.items = data['items'].map { |item_data| MusicAlbum.from_json(item_data) }
     genre
   end
